091a63dbcf7301aacf5c3645b433f89c3681b5d4
Dan Goodman <danthegoodman@icloud.com>
2021-12-21 09:31:44 -0500

    Add some debugging
    Change-type: patch

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,4 +5,10 @@ find "$(dirname $INPUT)" -maxdepth 2
 echo "output contents at $OUTPUT:"
 find "$(dirname $OUTPUT)" -maxdepth 2
 
+echo "my working dir"
+pwd
+
+echo "ls my dir"
+ls .
+
 node ./build/index.js
