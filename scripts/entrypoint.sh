4c209c21cd4f650782f617e0bf32367b8350f8af
Dan Goodman <danthegoodman@icloud.com>
2021-12-21 09:37:39 -0500

    ls build dir
    Change-type: patch

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,10 +5,8 @@ find "$(dirname $INPUT)" -maxdepth 2
 echo "output contents at $OUTPUT:"
 find "$(dirname $OUTPUT)" -maxdepth 2
 
-echo "my working dir"
-pwd
 
-echo "ls my dir"
-ls .
+echo "ls build dir"
+ls ./build
 
 node ./build/index.js
